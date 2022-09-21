b3b547c68d2c4ae782cf4da9d159e45ecea6f4cd
PakkoGraphic <41129374+pakkographic@users.noreply.github.com>
2022-09-21 22:08:21 +0200

    removing test files, tokens are invalids tho haha

--- test.mjs
+++
@@ -1,9 +0,0 @@
-import TouchGuild from '.'
-
-const client = new TouchGuild.Client({'token': 'token here'})
-
-client.connect()
-
-client.on('messageCreate', (msg)=> {
-    msg.createMessage({content: 'yikes!'})
-})
