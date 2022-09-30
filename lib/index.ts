c5941bc14d3f90c0c3fe0b576c42828fd6190c78
PakkoGraphic <41129374+pakkographic@users.noreply.github.com>
2022-09-30 21:54:22 +0200

    edit exports

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,6 @@ export * from './structures/Client'
 export * from './structures/Message'
 export * from './structures/Channel'
 export * from './structures/Member'
-export * from './structures/Message'
 export * from './structures/Guild'
 export * from './structures/User'
 
