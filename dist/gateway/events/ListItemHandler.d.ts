e6f922c6908c451ea5e643a4d73f556fdbd9b8e1
PakkoGraphic <41129374+pakkographic@users.noreply.github.com>
2022-10-22 11:09:36 +0200

    Delete dist directory

--- dist/gateway/events/ListItemHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import { GatewayEventHandler } from "./GatewayEventHandler";
-export declare class ListItemHandler extends GatewayEventHandler {
-    listItemCreate(data: object): void;
-    listItemUpdate(data: object): void;
-    listItemDelete(data: object): void;
-    listItemComplete(data: object): void;
-    listItemUncomplete(data: object): void;
-}
--- dist/gateway/events/MessageHandler.d.ts
+++
@@ -1,9 +0,0 @@
-import { messageReactionRawTypes } from "../../Types";
-import { GatewayEventHandler } from "./GatewayEventHandler";
-export declare class MessageHandler extends GatewayEventHandler {
-    messageCreate(data: object): Promise<boolean>;
-    messageUpdate(data: object): boolean;
-    messageDelete(data: object): boolean;
-    messageReactionAdd(data: messageReactionRawTypes | any): boolean;
-    messageReactionRemove(data: messageReactionRawTypes | any): boolean;
-}
