be07fe5f588054ec0c19385e7ba1251ca7dcb72e
PakkoGraphic <41129374+pakkographic@users.noreply.github.com>
2022-11-20 21:55:07 +0100

    fix

--- scripts/should-publish.js
+++ scripts/should-publish.js
@@ -1,10 +1,17 @@
 const { execSync } = require("child_process");
 const d = Date.now();
 const date = new Date(execSync("npm view touchguild time.modified --json").toString().slice(1,-2));
-
-if((d - date.getTime()) > 3.6e+6) {
+const latestDev = execSync("npm view touchguild dist-tags.dev --json").toString().slice(1,-2).split(".").slice(-1)[0];
+const latestLocal = execSync("git rev-parse --short HEAD").toString().slice(0, -1);
+console.log("Latest Published:", latestDev);
+console.log("Latest Local:", latestLocal);
+if(latestDev === latestLocal) {
+	console.log("Latest published matches local, a publish should not happen.")
+	process.exit(1);
+}
+if((d - date.getTime()) > 3600000) {
 	// more than an hour, publish
-	console.log("A publish hasn't happened within the last hour, a publish can happen.");
+	console.log("A publish has not happened within the last hour, a publish can happen.");
 	process.exit(0);
 } else {
 	// less than an hour, no publish
