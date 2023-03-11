95eac831c4e808bdcf11daae0292f42c68670cd0
pakkographic <41129374+pakkographic@users.noreply.github.com>
2023-03-11 22:39:12 +0100

    v1.2.2 release.
    https:://touchguild.com/updates/1.2.2

--- pkgconfig.ts
+++ pkgconfig.ts
@@ -2,7 +2,7 @@ import * as pkg from "./package.json";
 import { GatewayVersion, GatewayURL } from "guildedapi-types.ts/v1";
 export const config = {
     name:          "TouchGuild",
-    branch:        "Development build",
+    branch:        "Stable build",
     version:       pkg.version,
     NodeJSVersion: process.version,
     GuildedAPI:    {
