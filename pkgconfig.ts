9eef3973003f36dff3069e93678993e624130f8b
pakkographic <41129374+pakkographic@users.noreply.github.com>
2023-03-12 13:59:54 +0100

    Stable build -> Dev build

--- pkgconfig.ts
+++ pkgconfig.ts
@@ -2,7 +2,7 @@ import * as pkg from "./package.json";
 import { GatewayVersion, GatewayURL } from "guildedapi-types.ts/v1";
 export const config = {
     name:          "TouchGuild",
-    branch:        "Stable build",
+    branch:        "Development build",
     version:       pkg.version,
     NodeJSVersion: process.version,
     GuildedAPI:    {
